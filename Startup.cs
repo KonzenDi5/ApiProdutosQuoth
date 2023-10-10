c6a7acbeb3b7a083dd6d2fe014d466e8b2279f52
Lucas Konzen <104283188+KonzenDi5@users.noreply.github.com>
2023-10-10 10:57:58 -0300

    comments

--- Startup.cs
+++ Startup.cs
@@ -12,10 +12,10 @@ namespace ProdutosQuoth
 
         public IConfiguration Configuration { get; }
 
-        // Este é o método ConfigureServices onde você configura os serviços da aplicação.
+        //  método ConfigureServices  configura os serviços da aplicação
         public void ConfigureServices(IServiceCollection services)
         {
-            // Configuração de outros serviços...
+    
 
             services.AddDbContext<ProdutoContext>(options =>
             {
@@ -27,12 +27,9 @@ namespace ProdutosQuoth
             // Configuração do MediatR
             services.AddMediatR(typeof(Startup));
 
-            // Configuração de outros serviços...
-
-            // Certifique-se de adicionar outras configurações e serviços necessários aqui
         }
 
-        // Este é o método Configure onde você configura o pipeline de middleware HTTP.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,12 +38,12 @@ namespace ProdutosQuoth
             }
             else
             {
-                // Configure o tratamento de erros para ambientes de produção, se necessário.
+                // tratamento de erros para ambientes de produção, se necessário.
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts(); // Ativar o Strict Transport Security (HSTS)
             }
 
-            // Configuração do middleware para roteamento de controllers
+            // middleware para roteamento de controllers
             app.UseRouting();
 
             // Middleware para forçar o uso de HTTPS
